ed092e366502fa34e0e4e9c2acb3f530366070d3
antho6229 <antho6229@7L0M6Y1.Student.UGDSB.ED>
2020-02-20 13:57:44 -0500

    a

---
+++ src/palindrome/Palindrome.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package palindrome;
+
+/**
+ *
+ * @author antho6229
+ */
+
+import javax.swing.JOptionPane;
+
+public class Palindrome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String forward, backwards;
+        
+        forward = JOptionPane.showInputDialog("Words that are arranged the same way forwards as they are backwards are called palindromes.\nThis program determines if a word is a palindrome.\n\nEnter a word:");
+    }
+    
+}
